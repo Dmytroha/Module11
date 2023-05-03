d1cedf9539a8d84f54b1b2640cbb27f3ce688038
Dmytro Milovanov <milovanovu@gmail.com>
2023-05-03 20:59:12 +0200

    Complete Task3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,13 +5,18 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Task1
         List<String> names = List.of("Ivan", "Maria", "Peter", "Olga","Alina","Tonny","Tatty", "John","Daniel");
         System.out.println((new nameFilterByIndex()).filterNames(names)); // 1. Ivan, 3. Peter
 
 
+        //Task2
         List<String> input = List.of("foo", "bar", "baz","boo", "jar", "jazz");
         System.out.println((new toUppercaseAndSort(input)).transform());
+        //Task3
+        String[] stringArray = {"1, 2, 0", "4, 5"};
+        System.out.println((new StringArrayToStringWithDigit( stringArray)).tranform());
+
 
 
     }
---
+++ src/main/java/org/example/StringArrayToStringWithDigit.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class StringArrayToStringWithDigit {
+    private final String[] inputStringsArray;
+
+    public StringArrayToStringWithDigit(String[] inputStringsArray) {
+               this.inputStringsArray = inputStringsArray;
+    }
+
+    public String tranform() {
+        String result = Arrays.stream(inputStringsArray) // string array to strings stream
+                .flatMap((s)->Arrays.stream(s.split(", "))) // get string from stream, split to new string array {"1","2","0"}
+                .map(Integer::valueOf) // convert every element to int
+                .sorted() // sort array of Integer in natural order
+                .map(String::valueOf) // convert every Integer to string
+                .collect(Collectors.joining(", ")); // join string elements of stream with ", " delimiter
+
+        return result; //return result string
+    }
+
+
+}
+
+
